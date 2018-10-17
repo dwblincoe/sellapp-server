ca941c71c9d3cf756d16a94ddf75af7d8e44ab82
Drew Blincoe <Drew@Drews-MacBook-Pro.local>
2018-10-17 16:19:35 -0400

    sellappserver1.3

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(`postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/sellapplog`, {
     dialect: 'postgres'
 })
 
@@ -12,8 +12,3 @@ sequelize.authenticate().then(
 )
 
 module.exports = sequelize
-
-
-
-
-
